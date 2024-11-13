c3a459618296285d512002556e36b63ef66fe4c5
KGxALIHAN <ulugbekovalihan1@gmail.com>
2024-11-13 17:10:12 +0600

    подключил other

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 import os 
 from dotenv import load_dotenv
 from handlers.start import start_kb
-
+from handlers.other import start_router
 load_dotenv()
 bot_token = os.getenv('BOT_TOKEN')
 
@@ -14,10 +14,9 @@ bot = Bot(token=bot_token)
 
 dp = Dispatcher()
 
-
-
 async def main():
     dp.include_router(start_kb)
+    dp.include_router(start_router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
